43358a30078073fa4bd79e709026c692d3ebcbb5
Hussein Khaled <hessenkhaled0@gmail.com>
2022-06-04 15:47:56 -0700

    Ex1_C_Program_To_Print_Sentence.c

---
+++ Unit_2_C_Programming/1_C_Basics_assignments/Ex1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,15 @@
+/*
+ ======================================================================================================================
+ Name        : EX1_C_Program_To_Print_Sentence.c
+ Author      : Hussein Khaled
+ Created on	 : Jul 4, 2022
+ Description : Unit 2, lesson_3 C-Progtamming => EX_1
+ ======================================================================================================================
+ */
+#include "stdio.h"
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
